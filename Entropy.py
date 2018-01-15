68436442995c3c8d6a4c9737089894ce7b73cd07
Denis Khryashchev <dk2926@nyu.edu>
2018-01-15 14:04:18 -0500

    Optimized version, 4x faster

--- Entropy.py
+++ Entropy.py
@@ -7,20 +7,35 @@
     Given the time series its time-correlated entropy S (real value) is evaluated.
 '''
 
-import math
 import numpy as np
 
-def containsSublist(list_, sublist):
-    # Tests if the subsequence is present in the sequence.
-    # inputs:
-    #   list_: sequence of numbers,
-    #   sublist: subsequence of numbers.
+def is_sublist(list_, sublist):
+    # Turns string lists into strings and checks if the sublist is in the list
+    # input: 
+    #   list_ : list of strings [' ']
+    #   sublist : list of strings [' ']
     # output:
-    #   boolean value, True if the subsequence is in the sequence, False otherwise.
-    for i in xrange(0, len(list_)-len(sublist)+1):
-        if list_[i: i+len(sublist)] == sublist:
-            return True
+    #   True if sublist is in list_, False otherwise
+    l1 = "".join(map(str, list_))
+    l2 = "".join(map(str, sublist))
+    if l2 in l1:
+        return True
     return False
+    
+def shortest_subsequence(series, i):
+    # Calculates length of the shortest subsequence at time step i
+    # that hasn't appeared before
+    # input: 
+    #   series: time series, list of strings [' ']
+    #   i: time step index, integer
+    # output:
+    #   length of the shortest subsequence 
+    sequences = [series[i]]  
+    count = 1
+    while is_sublist(series[:i], sequences) and i + count <= len(series) - 1:
+        sequences = sequences + [series[i+count]]
+        count +=1
+    return len(sequences)
 
 def RealEntropy(timeseries):
     # Calculates an approximation of the time-correlated entropy.
@@ -28,14 +43,8 @@ def RealEntropy(timeseries):
     #   timeseries: sequence of numbers.
     # output:
     #   RealEntropy: approximation of the time-correlated entropy of the time series.
-    Shortest_Substring_Length = [1]
-    for i in range(1, len(timeseries)):
-        sequences = [timeseries[i]]  
-        count = 1
-        while containsSublist(timeseries[:i], sequences) and i + count <= len(timeseries) - 1:
-            sequences.append(timeseries[i+count])
-            count +=1
-        Shortest_Substring_Length.append(len(sequences))
-    RealEntropy = math.log(len(timeseries)) * len(timeseries) / sum(Shortest_Substring_Length)
+    timeseries = map(str, timeseries)
+    substring_length_gen = (shortest_subsequence(timeseries, i) for i in range(1, len(timeseries)))
+    shortest_substring_lengths = [1] + map(lambda length: length, substring_length_gen)
+    RealEntropy = np.log(len(timeseries)) * len(timeseries) / np.sum(shortest_substring_lengths)
     return RealEntropy
-
